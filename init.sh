96d78911b4037376e2b0defbe8159e646e8805fe
Mike Chernev <mike@mikechernev.com>
2013-08-08 20:52:14 +0300

    Initial commit

---
+++ init.sh
@@ -0,0 +1,4 @@
+DIR="$( cd "$( dirname "$0" )" && pwd )" 
+ln -sf $DIR/git/gitconfig ~/.gitconfig
+ln -sf $DIR/vim/vimrc ~/.vimrc
+ln -sf $DIR/sublime3/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User
